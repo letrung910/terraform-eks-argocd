a194ef1e8edb1ec350e26b46cf670d6c04ef54bd
Trung Le Van <letrung910@gmail.com>
2023-07-01 20:22:34 +0700

    init workflow for main branch

---
+++ storage/variable.tf
@@ -0,0 +1,10 @@
+### define variable su dung
+variable "project" {
+  type    = string
+  default = "trunglvworkshop1111111"
+}
+variable "environment" {
+  type    = string
+  default = "kgmmmmmm"
+}
+
