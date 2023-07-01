90d5c049008573c0169e7445fe5dc9509237a53e
Trung Le Van <letrung910@gmail.com>
2023-07-01 19:57:04 +0700

    init workflow for main branch

--- terraform.sh
+++ terraform.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ### example
 ### export ENVIRONMENT="dev"
+
 ### ./terraform.sh $ENVIRONMENT networking init
 ### ./terraform.sh $ENVIRONMENT networking plan
 ### ./terraform.sh $ENVIRONMENT networking apply --auto-approve
@@ -8,8 +9,9 @@
 ENVIRONMENT=${1}
 CHDIR=${2}
 TF_COMMAND=${3}
-TF_OPTS=$@
-
+TF_OPTS=${4}
+TF_OPTS2=${5}
+TF_OPTS3=${6}
 TF_VAR_ENVIRONMENT=${ENVIRONMENT:-dev}
 TF_VAR_region=${region:-us-east-1}
 
@@ -18,11 +20,11 @@ terraform {
 backend "s3" {
     bucket         = "trunglv-terraform-workshop"
     key            = "${ENVIRONMENT}/${CHDIR}/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "${TF_VAR_region}"
     encrypt        = true
     dynamodb_table = "${ENVIRONMENT}-dynamodb-lock"
 }
 }
 EOF
 
-terraform -chdir=${CHDIR} ${TF_COMMAND} -var-file="${ENVIRONMENT}.tfvars" ${TF_OPTS}
+terraform -chdir=${CHDIR} ${TF_COMMAND} -var-file="${ENVIRONMENT}.tfvars" ${TF_OPTS} ${TF_OPTS2} ${TF_OPTS3}
